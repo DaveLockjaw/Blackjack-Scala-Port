e62290ab108ea1f52154859420503c78a0f793eb
aaron.hamilton <aaron.hamilton@mymail.champlain.edu>
2019-04-07 21:18:44 -0400

    calculateScore function works

--- src/Blackjack.scala
+++ src/Blackjack.scala
@@ -4,13 +4,42 @@ import util.control.Breaks._
 
 object Blackjack {
 
-  //def calculateScore(cards: List[Any]) : Int = {
-  //  var score : Int = 0
-  //  var hasAce = false
-//
-  //
-//
-  //}
+def calculateScore(cards: List[Any]) : Int = {
+  var score : Int = 0
+  var scoreInc: Int = 0
+  var hasAce: Boolean = false
+
+  for (i <- cards)
+    {
+      i match {
+        case "2" => scoreInc = 2
+        case "3" => scoreInc = 3
+        case "4" => scoreInc = 4
+        case "5" => scoreInc = 5
+        case "6" => scoreInc = 6
+        case "7" => scoreInc = 7
+        case "8" => scoreInc = 8
+        case "9" => scoreInc = 9
+        case "10" => case "J" => case "Q" => case "K" => scoreInc = 10
+        case "A" => hasAce = true
+        case nil => break
+      }
+
+      if (hasAce)
+        {
+          if (score > 11)
+            scoreInc = 1
+          else
+            scoreInc = 11
+        }
+
+      score += scoreInc
+    }
+
+  println("score: ", score)
+
+  return score
+}
 
   def printStatus(playerCards: List[Any], dealerCards: List[Any]): Unit = {
     println("")
@@ -26,6 +55,8 @@ object Blackjack {
 
   def main(args: Array[String]) = {
     val deck = List[String]("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+    calculateScore(deck)
+
     val playerCards = List[String]()
     val dealerCards = List[String]()
 
@@ -45,6 +76,8 @@ object Blackjack {
     val newDeck3 = newDeck2.dropRight(1)
     printStatus(a, x)
 
+    println(calculateScore(z))
+
     while(true) { // player decision loop
       println("Do you want to (H)it", "(S)tay, or (Q)uit")
       val selection = readLine()
@@ -57,20 +90,21 @@ object Blackjack {
 
         if (calculateScore(b) > 21) {
           println("You busted! You lost!")
-          return 0
+          break
         }
       } else if (upperCaseSelection == "S") { //stay
         break
       } else if (upperCaseSelection == "Q") { //quit
-        return 0
+        break
       }
     }
 
+
     println ("Dealer draws rest of cards.")
     //keep drawing cards till 17
-    while (calculateScore(x) > 21) {
+//    while (calculateScore(x) > 21) {
 
-    }
-    printStatus(b, x)
+//    }
+//    printStatus(b, x) // ***************
   }
 }
