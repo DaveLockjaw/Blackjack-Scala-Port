889c16e74939f472c585df4efc1a90d34f17fcee
aaron.hamilton <aaron.hamilton@mymail.champlain.edu>
2019-04-08 11:34:08 -0400

    boom

--- src/Blackjack.scala
+++ src/Blackjack.scala
@@ -5,13 +5,12 @@ import scala.collection.mutable.ListBuffer
 
 object Blackjack {
 
-def calculateScore(cards: List[Any]) : Int = {
-  var score : Int = 0
-  var scoreInc: Int = 0
-  var hasAce: Boolean = false
+  def calculateScore(cards: List[Any]): Int = {
+    var score: Int = 0
+    var scoreInc: Int = 0
+    var hasAce: Boolean = false
 
-  for (i <- cards)
-    {
+    for (i <- cards) {
       i match {
         case "2" => scoreInc = 2
         case "3" => scoreInc = 3
@@ -21,108 +20,153 @@ def calculateScore(cards: List[Any]) : Int = {
         case "7" => scoreInc = 7
         case "8" => scoreInc = 8
         case "9" => scoreInc = 9
-        case "10" => case "J" => case "Q" => case "K" => scoreInc = 10
+        case "10"  | "J" | "Q" | "K" => scoreInc = 10
         case "A" => hasAce = true
-        case nil => println("card is nil")
+        case _ => println("card is nil")
       }
 
-      if (hasAce)
-        {
-          if (score > 11)
-            scoreInc = 1
-          else
-            scoreInc = 11
-        }
+      if (hasAce) {
+        if (score > 11)
+          scoreInc = 1
+        else
+          scoreInc = 11
+      }
 
       score += scoreInc
     }
 
-  println("score: ", score)
-
-  return score
-}
+    return score
+  }
 
   def printStatus(playerCards: List[Any], dealerCards: List[Any]): Unit = {
     println("")
     println("Player's total is " + playerCards)
 
-    playerCards.foreach {println}
+    playerCards.foreach {
+      println
+    }
     println("")
 
     println("Dealer's total is " + dealerCards)
-    dealerCards.foreach {println}
+    dealerCards.foreach {
+      println
+    }
     println("")
   }
 
-  def main(args: Array[String]) = {
-    val deck = List[String]("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
-    calculateScore(deck)
+  def populateDeck(): ListBuffer[String] = {
+    var tmp = new ListBuffer[String]()
 
-    val playerCards = List[String]()
-    val dealerCards = List[String]()
+    tmp += "2"
+    tmp += "3"
+    tmp += "4"
+    tmp += "5"
+    tmp += "6"
+    tmp += "7"
+    tmp += "8"
+    tmp += "9"
+    tmp += "10"
+    tmp += "J"
+    tmp += "Q"
+    tmp += "K"
+    tmp += "A"
+
+    return tmp
+  }
+
+  def main(args: Array[String]) = {
+    var deckBuffer = populateDeck()
+    var playerDeckBuffer = new ListBuffer[String]()
+    var dealerDeckBuffer = new ListBuffer[String]()
 
     //Shuffle the deck randomly
-    val shuffledDeck = Random.shuffle(deck)
-    println(shuffledDeck)
+    var shuffledDeck = Random.shuffle(deckBuffer.toList)
+    println("Deck: " + shuffledDeck.mkString(", "))
 
+    // dealer draws first card
     println("Dealer draws first card. \n")
-    val dealerCardDeck = dealerCards ++ shuffledDeck.last
-    val newDeck = shuffledDeck.dropRight(1)
+    dealerDeckBuffer += shuffledDeck.last
+    shuffledDeck = shuffledDeck.dropRight(1)
+    println("Dealer's hand: " + dealerDeckBuffer.toList.mkString(", ") + "\n")
 
+    println("Current deck: " + shuffledDeck.mkString(", ") + "\n")
+    // player draws 2 cards
     println("Player receives two cards")
-    val y = playerCards ++ newDeck.last
-    val newDeck2 = newDeck.dropRight(1)
-    val z = playerCards ++ newDeck2.last
-    val a = y ++ z
-    val newDeck3 = newDeck2.dropRight(1)
-    printStatus(a, dealerCardDeck)
+    playerDeckBuffer += shuffledDeck.last
+    shuffledDeck = shuffledDeck.dropRight(1)
+    playerDeckBuffer += shuffledDeck.last
+    shuffledDeck = shuffledDeck.dropRight(1)
+    println("Your hand: " + playerDeckBuffer.toList.mkString(", ") + "\n")
 
-    //println(calculateScore(z))
+    var playerDraw: Boolean = true
+    var quit: Boolean = false
+    var playerBUSS = false
+    var dealerBUSS = false
 
-    var playerCardDeck = List[Any]()
-    while(true) { // player decision loop
+    while (playerDraw) { // player decision loop
       println("Do you want to (H)it", "(S)tay, or (Q)uit")
       val selection = readLine()
       val upperCaseSelection = selection.toUpperCase()
 
-        if (upperCaseSelection == "H") { //hit
-          playerCardDeck = playerCards ++ newDeck3.last
+      if (upperCaseSelection == "H") { //hit
+        playerDeckBuffer += shuffledDeck.last
+        shuffledDeck = shuffledDeck.dropRight(1)
 
-          printStatus(playerCardDeck, dealerCardDeck)
+        println("Your hand: " + playerDeckBuffer.toList.mkString(", ") + "\n")
 
-          if (calculateScore(playerCardDeck) > 21) {
-            println("You busted! You lost!")
-            break
-          }
-        } else if (upperCaseSelection == "S") { //stay
-          break
-        } else if (upperCaseSelection == "Q") { //quit
-          break
+        println("Your current score: " + calculateScore(playerDeckBuffer.toList))
+        if (calculateScore(playerDeckBuffer.toList) > 21) {
+          println("You busted! You lost!")
+          playerDraw = false
+          playerBUSS = true
         }
+      } else if (upperCaseSelection == "S") { //stay
+        playerDraw = false
+      } else if (upperCaseSelection == "Q") { //quit
+        playerDraw = false
+        quit = true
+      }
+    }
+
+    //keep drawing cards till 17
+    if (!quit) {
+      println("Dealer draws rest of cards.")
+      while (calculateScore(dealerDeckBuffer.toList) < 17) {
+        shuffledDeck = shuffledDeck.dropRight(1)
+
+        dealerDeckBuffer += shuffledDeck.last
+        shuffledDeck = shuffledDeck.dropRight(1)
+
+        println("Dealers hand: " + dealerDeckBuffer.toList.mkString(", ") + "\n")
       }
 
+      if (calculateScore(dealerDeckBuffer.toList) > 21) {
+        dealerBUSS = true
+        println("Dealer busts! You Win!")
+      }
 
-    println ("Dealer draws rest of cards.")
-    //keep drawing cards till 17
-    var deckBuffer = new ListBuffer[String]()
-    while (calculateScore(dealerCardDeck) < 17) {
-      val newDeck4 = newDeck3.dropRight(1)
-
-      deckBuffer += newDeck4.last
-      newDeck4.dropRight(1)
-      println(newDeck4)
+      if (dealerBUSS | playerBUSS) {
+        if (dealerBUSS & playerBUSS) {
+          println("What's wrong with y'all?")
+        }
+        else if (dealerBUSS) {
+          println("The dealer busted")
+        }
+        else if (playerBUSS) {
+          println("You busted")
+        }
+      } else {
+        if (calculateScore(dealerDeckBuffer.toList) > calculateScore(playerDeckBuffer.toList)) {
+          println("Dealer wins!")
+        } else if (calculateScore(dealerDeckBuffer.toList) < calculateScore(playerDeckBuffer.toList)) {
+          println("You Win!")
+        } else {
+          println("It's a tie!")
+        }
+      }
     }
-    val toListDeck = deckBuffer.toList
-    printStatus(playerCardDeck, toListDeck) // ***************
-
-    if (calculateScore(toListDeck) > 21){
-      println("Dealer busts! You Win!")
-    } else if (calculateScore(toListDeck) > calculateScore(playerCardDeck)) {
-      println("Dealer wins!")
-    } else if (calculateScore(toListDeck) < calculateScore(playerCardDeck)) {
-      println("You Win!")
-    } else{
-      println("It's a tie!")
+    else {
+      println("wOW WAy tO RaGe QuIt")
     }
   }
 }
